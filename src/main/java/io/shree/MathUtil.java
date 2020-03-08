44516a311c2f3ab5e7e132a731308bcf2a4be35b
Srikanthnamashivaya <srikanth.namashivaya@cognizant.com>
2020-03-09 00:17:22 +0530

    This is my first git commit

---
+++ src/main/java/io/shree/MathUtil.java
@@ -0,0 +1,8 @@
+package io.shree;
+
+public class MathUtil {
+
+	public int add(int a , int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/io/shree/MathUtilTest.java
@@ -0,0 +1,14 @@
+package io.shree;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilTest {
+
+	@Test
+	void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
